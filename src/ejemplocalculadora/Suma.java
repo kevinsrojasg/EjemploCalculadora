f06f145d6db1e3248ddce43889907a4045ba8524
kevinsrojasg <kevinsrojasg@gmail.com>
2018-11-01 20:34:34 -0500

    EjemploCalculadora

---
+++ src/ejemplocalculadora/Suma.java
@@ -0,0 +1,10 @@
+package ejemplocalculadora;
+
+public class Suma {
+
+    public static double suma(double sumando1, double sumando2) {
+        double resultado = sumando1 + sumando2;
+        return resultado;
+    }
+
+}
